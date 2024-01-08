f24c6e0a5326d1160892a7b3425914bed74e0d21
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-01-07 21:17:27 -0600

    calculated distance to Moon and Mars

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,26 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let kilometersToMars = 225000000;
+let kilometersToMoon = 384400;
+const milesPerKilometer = 0.621;
+
+console.log(typeof 'Determination');
+console.log(typeof 17500);
+console.log(typeof 225000000);
+console.log(typeof 384400);
+console.log(typeof 0.621);
+
+let milesToMars = kilometersToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
+
+let milesToMoon = kilometersToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
