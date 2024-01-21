0ae0b6893af87a252cb528dbd471f57282641386
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-01-21 17:39:43 -0600

    debugging exercise 4

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -11,27 +11,28 @@ let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+
+   if (crewStatus) {
+       console.log('Crew status cleared.');
+
+       if (computerStatus === 'green') {
+           console.log('Computer status cleared.');
+           launchReady = true;
+       } else {
+           console.log('WARNING: Computer not ready!');
+       }
+   } else {
+       console.log('WARNING: Crew not ready!');
+   }
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
 }
 
 console.log("launchReady = ", launchReady);
 
-if (crewStatus && computerStatus === 'green'){
-   console.log('Crew & computer cleared.');
-   launchReady = true;
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
 } else {
-   console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   console.log('Launch scrubbed.');
 }
-
-console.log("launchReady = ", launchReady);
-
-// if (launchReady) {
-//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
-//    console.log('Liftoff!');
-// } else {
-//    console.log('Launch scrubbed.');
-// }
