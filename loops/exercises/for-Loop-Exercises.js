bf72267c4b9f3017722e767aad97ce1749442d8e
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-01-21 20:00:22 -0600

    for loops exercise completed

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,21 +4,72 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+// part a
+for (let i = 0; i < 21; i++) {
+  console.log(i);
+}
 
+// part b
+for (let i = 3; i <= 29; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// part c
+for (let i = 12; i >= -14; i--) {
+if ( i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// part d
+for (let i = 50; i >= 20; i--) {
+  if ( i % 3 === 0) {
+      console.log(i);
+    }
+  }
 
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
 
+// part a
+let launchString = 'LaunchCode';
+let launchArray = [1, 5, 'LC101', 'blue', 42];
 
+for (let i = 0; i < launchArray.length; i++){
+  console.log(launchArray[i]);
+}
 
+// part b
+for (let i = launchString.length - 1; i >= 0; i--){
+  console.log(launchString[i]);
+}
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  // part a
+  let numbers = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [];
+  let odds = [];
+
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] % 2 === 0){
+      evens.push(numbers[i]);
+    } else {
+      odds.push(numbers[i]);
+    }
+  }
+
+  // part b
+
+  console.log('Even numbers: ', evens);
+  console.log('Odd numbers: ', odds);
