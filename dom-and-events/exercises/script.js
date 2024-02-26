68322a5861cd26c2b8b62375285938ba11e716af
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-02-26 16:32:49 -0600

    dom and events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,26 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+    const liftoffButton = document.getElementById("liftoffButton");
+    const statusReport = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    liftoffButton.addEventListener('click', function() {
+        statusReport.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('click', function() {
+        let confirmAbort = confirm("Are you sure you want to abort the mission?");
+        if (confirmAbort) {
+            statusReport.innerHTML = 'Mission aborted! Space shuttle returning home.';
+        }
+    });
+
+    missionAbort.addEventListener('mouseover', function() {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', function() {
+        missionAbort.style.backgroundColor = ''; 
+    });
 }
 
 window.addEventListener("load", init);
